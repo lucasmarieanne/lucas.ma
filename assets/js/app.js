3772c60ee36a3a0642e27b26f608121316c4d4ca
Lucas marie anne <pro.lucasmarieanne@gmail.com>
2022-05-20 16:54:26 +0200

    mise à jour

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
-// window.addEventListener('scroll', function () {
-//     let header = document.querySelector('header');
-//     let windowPosition = window.scrollY > 0;
+window.addEventListener('scroll', function () {
+    let header = document.querySelector('header');
+    let windowPosition = window.scrollY > 0;
   
-//     header.classList.toggle('scrolling-active', windowPosition);
-//   })
+    header.classList.toggle('scrolling-active', windowPosition);
+})
